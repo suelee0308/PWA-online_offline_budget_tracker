50d3f81ae888d6900fdb9293a1debaec3258078c
Sue Lee <sue.lee0308@gmail.com>
2021-11-16 13:26:23 -0600

    indexeddb added

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
   ];
 
-  const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
